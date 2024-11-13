af9009ddf89b5c8cf2c5a55886c9a77bed45a8ee
Fernando Spelling <fernando.spelling@etechss.com.br>
2024-11-13 10:23:30 -0300

    ajsute

--- Poc.ShopCqrs/Poc.ShopCqrs.Bus/EventBus.cs
+++ Poc.ShopCqrs/Poc.ShopCqrs.Bus/EventBus.cs
@@ -1,12 +1,14 @@
 ﻿using MediatR;
+using Poc.ShopCqrs.Domain.Core.Events;
 using Poc.ShopCqrs.Domain.Core.Messaging;
 using Poc.ShopCqrs.Domain.Interfaces.EventBus;
 
 namespace Poc.ShopCqrs.Bus
 {
-    public class EventBus(IMediator mediator) : IEventBus
+    public class EventBus(IMediator mediator, IEventStore eventStore) : IEventBus
     {
         private readonly IMediator _mediator = mediator;
+        private readonly IEventStore _eventStore = eventStore;
 
         public async Task<TResponse> SendQuery<TResponse>(IRequest<TResponse> query)
             => await _mediator.Send(query);
@@ -16,7 +18,7 @@ namespace Poc.ShopCqrs.Bus
 
         public async Task Publish<TEvent>(TEvent @event) where TEvent : Event
         {
-            //_eventStore.Save(@event);
+            _eventStore.Save(@event);
             await _mediator.Publish(@event!);
         }
     }
